4b84df8e2dc5ae08f6c1a22dd799467294cfe9de
tejassateesh <tejas.sateesh@nyu.edu>
2022-03-09 12:58:36 -0500

    Database issue

--- nyunite/settings.py
+++ nyunite/settings.py
@@ -88,7 +88,10 @@ WSGI_APPLICATION = "nyunite.wsgi.application"
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {"ENGINE": "django.db.backends.sqlite3", "NAME": "nyuniteapp.sqlite3"}
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'nyunite.sqlite3',
+    }
 }
 
 # Password validation
