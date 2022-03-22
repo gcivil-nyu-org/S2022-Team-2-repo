5978044d97f87eca4a2ec76d2c8e7bade4843180
akshat-17 <60538557+akshat-17@users.noreply.github.com>
2022-03-22 16:27:54 -0400

    Signup UI minor changes

--- frontend/src/views/authentication/Signup.js
+++ frontend/src/views/authentication/Signup.js
@@ -14,8 +14,16 @@ import {
     Box,
   } from '@chakra-ui/react';
   import loginphoto from '../../assets/images/login.jpg';
+  import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
  
   export default function Signup() {
+    const history=useHistory();
+    const handleClickSignUp = () => {
+      history.push('/Signup');
+    }
+    const handleClickLogIn = () => {
+      history.push('/login');
+    }
     return (
       <Stack
       height={'865px'}
@@ -25,7 +33,7 @@ import {
       >
           
         <Flex flex={1} w='100%' height='100%' >
-          <Image width='100%' height='860px'  alt={'Login Image'} objectFit={'fill'} src={loginphoto}   />
+          <Image width='100%' height='860px'  alt={'SignUp Image'} objectFit={'fill'} src={loginphoto}   />
         </Flex>
         
         <Flex
@@ -35,14 +43,16 @@ import {
           justify={'center'}
           bgColor={'#230542'}
           >
-          <Box bg="white" w="50%" p={0} color="black" borderRadius="lg">
-            <Stack spacing={4} w={'full'} maxW={'md'}>
-              <Box  w='100%' bg="#a69db2" borderRadius="lg" p={3}>
-                <Heading align={'center'} fontSize={'xl'}>
-                  Sign Up
-                </Heading>
-              </Box>
-            </Stack>
+          <Box bg="white" h='48%' w='50%' p={0} color="black" borderRadius="lg">
+
+          <Flex color={'black'} >       
+            <Box as='button' borderColor={'white'} borderWidth={3}   bg='#a69db2' borderRadius='5px 0px 0px 5px' p={3} onClick={handleClickSignUp} w={'50%'}_hover={{ bg:'#D6BCFA'}}>
+              <Heading fontSize={'xl'}>Sign Up</Heading>
+            </Box>
+            <Box as='button' bg='#c7d0d8' borderRadius='0px 5px 5px 0px' p={3} onClick={handleClickLogIn} w={'50%'}_hover={{ bg:'#D6BCFA'}}>
+              <Heading fontSize={'xl'}>Log In</Heading>
+            </Box>
+          </Flex>
   
             <Stack
               spacing={4}
@@ -58,6 +68,8 @@ import {
                   <Input
                     borderWidth={'2px'}
                     type="text"
+                    padding='8px'
+                    height="100%"
                     placeholder="NetID"
                     _placeholder={{ color: '#57068c' }}
                   />
@@ -65,15 +77,29 @@ import {
                 <FormControl id="Name" border={'#57068c'}>
                   <Input
                     borderWidth={'2px'}
-                    width="50%"
+                    height="100%"
+                    width="40%"
                     type="text"
+                    padding='8px'
                     placeholder="FirstName"
                     _placeholder={{ color: '#57068c' }}
                   />
                   <Input
                     borderWidth={'2px'}
+                    marginLeft='5px'
+                    padding='8px'
+                    height="100%"
+                    width="15%"
+                    type="text"
+                    placeholder="MI"
+                    _placeholder={{ color: '#57068c' }}
+                  />
+                  <Input
+                    borderWidth={'2px'}
                     width="40%"
-                    ml="10px"
+                    height="100%"
+                    ml="5px"
+                    padding='8px'
                     type="text"
                     placeholder="LastName"
                     _placeholder={{ color: '#57068c' }}
@@ -88,6 +114,8 @@ import {
                     borderWidth={'2px'}
                     type="password"
                     placeholder="Password"
+                    padding='8px'
+                    height="100%"
                     _placeholder={{ color: '#57068c' }}
                   />
                 </FormControl>
@@ -96,6 +124,8 @@ import {
                     borderWidth={'2px'}
                     type="password"
                     placeholder="Retype Password"
+                    padding='8px'
+                    height="100%"
                     _placeholder={{ color: '#57068c' }}
                   />
                 </FormControl>
