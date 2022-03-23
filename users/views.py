4b0b70d16435a974f6114571bb07f3f313cf61e8
Tejas <90651906+ts4044@users.noreply.github.com>
2022-03-23 17:50:34 -0400

    Release Sprint Week 7 (#65)
    * Preferences redirect
    
    * remove import
    
    * update UI on password reset
    
    * Fun buttons
    
    * home page buttom fix
    
    * More buttons
    
    * Preferences UI (#64)
    
    * Preferences Page ish
    
    * Formatting
    
    Co-authored-by: reganbragg <regan.bragg22@gmail.com>
    Co-authored-by: DarylC <dc4676@nyu.edu>

--- users/views.py
+++ users/views.py
@@ -13,13 +13,19 @@ from users.forms import (
     ResetPasswordRequestForm,
     ResetPasswordForm,
     PreferencesPersonalityForm,
-    PreferencesHobbiesForm,
     LoginForm,
 )
 
 from users.tokens import account_activation_token
 
 User = get_user_model()
+
+
+# def checkloggedinuser(request):
+#     if request.user is not None:
+#         user = request.user
+#         if user.is_authenticated():
+#             return HttpResponseRedirect("/dashboard")
 
 
 def home(request):
@@ -182,29 +188,30 @@ def preferences_personality(request):
         user = request.user
         if form.is_valid() and user is not None:
             prefs = form.save(commit=False)
+            user = User.objects.get(id=user.id)
             prefs.user = user
             prefs.save()
             print()
-            return HttpResponseRedirect("/preferences/page2")
-
-    return render(request, "users/preferences/preferences1.html", context_dict)
-
-
-def preferences_hobbies(request):
-    context_dict = {"form": None}
-    form = PreferencesHobbiesForm()
-
-    if request.method == "GET":
-        context_dict["form"] = form
-    elif request.method == "POST":
-        form = PreferencesHobbiesForm(request.POST)
-        context_dict["form"] = form
-        if form.is_valid():
-            cleaned_data = form.cleaned_data
-            print(cleaned_data)
             return HttpResponseRedirect("/dashboard")
 
     return render(request, "users/preferences/preferences1.html", context_dict)
+
+
+# def preferences_hobbies(request):
+#     context_dict = {"form": None}
+#     form = PreferencesHobbiesForm()
+#
+#     if request.method == "GET":
+#         context_dict["form"] = form
+#     elif request.method == "POST":
+#         form = PreferencesHobbiesForm(request.POST)
+#         context_dict["form"] = form
+#         if form.is_valid():
+#             cleaned_data = form.cleaned_data
+#             print(cleaned_data)
+#             return HttpResponseRedirect("/dashboard")
+#
+#     return render(request, "users/preferences/preferences1.html", context_dict)
 
 
 @login_required
