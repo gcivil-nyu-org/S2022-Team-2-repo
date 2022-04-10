2099148a6b6d6fdaefcfe1bb186625078c9e4a53
Tejas <ts4044@nyu.edu>
2022-04-09 23:58:47 -0400

    Heroku more settings

--- nyunite/asgi.py
+++ nyunite/asgi.py
@@ -1,22 +1,17 @@
 import os
 
-from django.core.asgi import get_asgi_application
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
-
+from django.core.asgi import get_asgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "nyunite.settings")
 django_asgi_app = get_asgi_application()
 
-application = None
+from django_private_chat2 import urls
 
-
-if __name__ == "__main__":
-    from django_private_chat2 import urls
-
-    application = ProtocolTypeRouter(
-        {
-            "http": django_asgi_app,
-            "websocket": AuthMiddlewareStack(URLRouter(urls.websocket_urlpatterns)),
-        }
-    )
+application = ProtocolTypeRouter(
+    {
+        "http": django_asgi_app,
+        "websocket": AuthMiddlewareStack(URLRouter(urls.websocket_urlpatterns)),
+    }
+)
