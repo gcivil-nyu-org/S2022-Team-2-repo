73e2b83e82538d4ef8bcef7a9b767d9c52a4f2e6
Tejas <90651906+ts4044@users.noreply.github.com>
2022-04-14 00:19:03 -0400

    Profile pictures to S3 (#112)
    * Profile pictures to S3
    
    * Testing default
    
    * Default Image

--- nyunite/settings.py
+++ nyunite/settings.py
@@ -123,6 +123,18 @@ else:
 
 DATABASES = {"default": database}
 
+
+# S3 settings
+AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
+AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME")
+AWS_S3_SIGNATURE_VERSION = "s3v4"
+AWS_S3_REGION_NAME = "us-east-1"
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
+AWS_S3_VERIFY = True
+DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
 
